d1784db84d26d78e07eaed9a782d44e0511e8918
whitefusion <whitefusion@163.com>
2017-09-14 00:15:35 +0800

    create deck and card view template

--- app/scripts/controllers/engine.js
+++ app/scripts/controllers/engine.js
@@ -11,29 +11,41 @@ angular.module('memoGameApp')
   .controller('EngineCtrl', function () {
     this.moves = 0;
     this.stars = 3;
+
     this.stopWatch = 0;
     this.subWatch = 0;
+
     this.width = 4;
     this.height = 4;
 
-    this.items = ["fa-bomb","fa-bicycle","fa-cube",
+    this.itemsCollection = ["fa-bomb","fa-bicycle","fa-cube",
     "fa-paper-plane-o","fa-leaf","fa-diamond",
     "fa-bolt","fa-anchor","fa-superpower",
     "fa-spoon","fa-plug","fa-legal",
     "fa-balance-scale","fa-bath","fa-birthday-cake",
     "fa-bug","fa-cog","fa-flighter-jet"];
 
-    this.randItems = {};
+    this.randItems = [];
+    this.randItemStatus = {};
 
     this.initialGame = function() {
         this.stopWatch = 0;
 
         const numItems = this.width*this.height/2;
-        let randTokens = randomPermute(this.items.length);
+        let randTokens = randomPermute(this.itemsCollection.length);
 
-        for (i in randTokens.slice(0,numItems))
-            this.randItems[this.items[i]]=false;
+        for (let i in randTokens.slice(0,numItems)){
+            let temp = this.itemsCollection[i];
+            this.randItems.push(temp);
+            this.randItems.push(temp);
+            this.randItemStatus[temp]=false;
+        }
+
+        shuffle(this.randItems);
     }
+
+
+    this.initialGame();
 
     function shuffle(o) {
         for(let j, x, i = o.length; i; j = parseInt(Math.random() * i), x = o[--i], o[i] = o[j], o[j] = x);
